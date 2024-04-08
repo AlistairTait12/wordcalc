3a00b738329bbc8d08446ee71eb9bb72da0ed4c9
Alistair Tait <alistair.tait1@gmail.com>
2024-04-08 20:00:07 +0100

    Added model builder to get a list of tiles from some custom notation format

---
+++ WordCalc.Logic.Tests/ModelBuilder/TileListBuilderTests.cs
@@ -0,0 +1,88 @@
+﻿using WordCalc.Logic.ModelBuilder;
+
+namespace WordCalc.Logic.Tests.ModelBuilder;
+
+[ExcludeFromCodeCoverage]
+[TestFixture]
+public class TileListBuilderTests
+{
+    private TileListBuilder _builder;
+
+    [SetUp]
+    public void Setup() => _builder = new TileListBuilder();
+
+    [Test]
+    public void BuildWithSimpleWordReturnsTileList()
+    {
+        // Arrange
+        var expectedTiles = new List<Tile>
+        {
+            new () { Letter = 'a' },
+            new () { Letter = 'p' },
+            new () { Letter = 'p' },
+            new () { Letter = 'l' },
+            new () { Letter = 'e' }
+        };
+
+        // Act
+        var actual = _builder.Build("apple");
+
+        // Assert
+        actual.Should().BeEquivalentTo(expectedTiles, options => options.WithStrictOrdering());
+    }
+
+    [Test]
+    public void BuildWithTileNotationReturnsTileList()
+    {
+        // Arrange
+        var expectedTiles = new List<Tile>
+        {
+            new () { Letter = 'a', TilePremium = TilePremium.DoubleLetter },
+            new () { Letter = 'p' },
+            new () { Letter = 'p' },
+            new () { Letter = 'l', TilePremium = TilePremium.TripleLetter },
+            new () { Letter = 'e' }
+        };
+
+        // Act
+        var actual = _builder.Build("a2ppl3e");
+
+        // Assert
+        actual.Should().BeEquivalentTo(expectedTiles, options => options.WithStrictOrdering());
+    }
+
+    [Test]
+    public void BuildWithTileNotationContainingBlankLetterReturnsWord()
+    {
+        // Of course in a real board, you wouldn't find these squares in this order
+        // Arrange
+        var expectedTiles = new List<Tile>
+        {
+            new () { Letter = 'a', TilePremium = TilePremium.DoubleLetter },
+            new () { Letter = 'p', IsBlank = true, TilePremium = TilePremium.DoubleWord },
+            new () { Letter = 'p' },
+            new () { Letter = 'l', TilePremium = TilePremium.TripleLetter },
+            new () { Letter = 'e' }
+        };
+
+        // Act
+        var actual = _builder.Build("a2p_+pl3e");
+
+        // Assert
+        actual.Should().BeEquivalentTo(expectedTiles, options => options.WithStrictOrdering());
+    }
+
+    [TestCase("appl!e")]
+    [TestCase("ban23ana")]
+    [TestCase("ora__nge")]
+    [TestCase("no spaces")]
+    [TestCase("")]
+    public void BuildWithInvalidNotationThrowsInvalidArgumentException(string input)
+    {
+        // Act
+        var act = () => _builder.Build(input);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage($"{input} is not valid notation");
+    }
+}
---
+++ WordCalc.Logic/ModelBuilder/TileListBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using WordCalc.Logic.Models;
+
+namespace WordCalc.Logic.ModelBuilder;
+
+public class TileListBuilder
+{
+    private const string _validTilePattern = @"[A-Za-z]{1}_{0,1}[23\+\*]{0,1}";
+
+    public IEnumerable<Tile> Build(string tileNotation)
+    {
+        ValidateNotation(tileNotation);
+
+        return Regex.Matches(tileNotation, _validTilePattern)
+            .ToList()
+            .Select(GetTileFromMatch);
+    }
+
+    private static void ValidateNotation(string tileNotation)
+    {
+        var validWordPattern = @$"^({_validTilePattern})+$";
+        if (!Regex.IsMatch(tileNotation, validWordPattern))
+        {
+            throw new ArgumentException($"{tileNotation} is not valid notation");
+        }
+    }
+
+    private static Tile GetTileFromMatch(Match match)
+    {
+        var premium = Regex.Match(match.Value, @"[23\+\*]{1}").Value;
+        var tilePremium = premium switch
+        {
+            "" => TilePremium.None,
+            "2" => TilePremium.DoubleLetter,
+            "3" => TilePremium.TripleLetter,
+            "+" => TilePremium.DoubleWord,
+            "*" => TilePremium.TripleWord,
+            _ => throw new ArgumentException($"{match.Value} is not valid notation")
+        };
+
+        return new Tile
+        {
+            Letter = match.Value[0],
+            IsBlank = match.Value.Contains('_'),
+            TilePremium = tilePremium
+        };
+    }
+}
