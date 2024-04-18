c301259f6c4b30b23a8601cbb5956f5e54f11afe
Alistair Tait <alistair.tait1@gmail.com>
2024-04-18 20:23:45 +0100

    Added a RoundComponentModel, PlayerTurnComponentModel and ModelBuilder with tests

---
+++ WordCalc.Presentation.Tests/ModelBuilders/RoundComponentModelListBuilderTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using System.Diagnostics.CodeAnalysis;
+using WordCalc.Logic.Models;
+using WordCalc.Presentation.ModelBuilders;
+using WordCalc.Presentation.ViewModel;
+
+namespace WordCalc.Presentation.Tests.ModelBuilders;
+
+[ExcludeFromCodeCoverage]
+[TestFixture]
+public class RoundComponentModelListBuilderTests
+{
+    [Test]
+    public void BuildRoundComponents_WhenCalled_ReturnsObservableCollectionOfRoundComponentModel()
+    {
+        // Arrange
+        var game = new Game()
+        {
+            Players =
+            [
+                new ()
+                {
+                    Name = "Dave",
+                    Turns =
+                    [
+                        new () { Words = [CreateWord("apple") ] },
+                        new () { Words = [CreateWord("grape") ] },
+                    ]
+                },
+                new ()
+                {
+                    Name = "Kim",
+                    Turns =
+                    [
+                        new () { Words = [CreateWord("banana") ] },
+                    ]
+                }
+            ]
+        };
+
+        var expected = new List<RoundComponentModel>
+        {
+            new ()
+            {
+                RoundNumber = 1,
+                PlayerTurns =
+                [
+                    new () { PlayerName = "Dave", Words = "apple", TurnScore = 9, CumulativeScore = 9 },
+                    new () { PlayerName = "Kim", Words = "banana", TurnScore = 8, CumulativeScore = 8 }
+                ]
+            },
+            new ()
+            {
+                RoundNumber = 2,
+                PlayerTurns =
+                [
+                    new () { PlayerName = "Dave", Words = "grape", TurnScore = 8, CumulativeScore = 17 }
+                ]
+            }
+        };
+
+        var roundComponentModelListBuilder = new RoundComponentModelListBuilder();
+
+        // Act
+        var actual = roundComponentModelListBuilder.BuildRoundComponents(game);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    // Untested logic in the tests. Yikes. \_(o_O)_/
+    private static Word CreateWord(string input) => new ()
+    {
+        Tiles = input.Select(c => new Tile { Letter = c }).ToList()
+    };
+}
---
+++ WordCalc.Presentation/ModelBuilders/RoundComponentModelListBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using WordCalc.Logic.Models;
+using WordCalc.Presentation.ViewModel;
+
+namespace WordCalc.Presentation.ModelBuilders;
+
+public class RoundComponentModelListBuilder
+{
+    public ObservableCollection<RoundComponentModel> BuildRoundComponents(Game game)
+    {
+        var maxTurns = game.Players.Max(p => p.Turns.Count);
+        var roundComponents = new ObservableCollection<RoundComponentModel>();
+
+        for (var i = 0; i < maxTurns; i++)
+        {
+            var roundComponent = new RoundComponentModel
+            {
+                RoundNumber = i + 1,
+                PlayerTurns = new ()
+            };
+
+            foreach (var player in game.Players)
+            {
+                if (player.Turns.Count > i)
+                {
+                    var turn = player.Turns[i];
+                    var playerTurnComponentModel = new PlayerTurnComponentModel
+                    {
+                        PlayerName = player.Name,
+                        Words = GetCommaSeparatedWords(turn.Words),
+                        TurnScore = turn.GetValue(),
+                        CumulativeScore = player.Turns.Take(i + 1).Sum(t => t.GetValue())
+                    };
+                    roundComponent.PlayerTurns.Add(playerTurnComponentModel);
+                }
+            }
+            roundComponents.Add(roundComponent);
+        }
+
+        return roundComponents;
+    }
+
+    private string GetCommaSeparatedWords(IEnumerable<Word> words)
+    {
+        var wordList = new List<string>();
+        foreach (var word in words)
+        {
+            var wordString = string.Join(string.Empty, word.Tiles.Select(tile => tile.Letter));
+            wordList.Add(wordString);
+        }
+        return string.Join(", ", wordList).TrimEnd();
+    }
+}
